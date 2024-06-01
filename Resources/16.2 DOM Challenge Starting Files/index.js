ff6df89e16c3ed99095647d4d1897a53d9a57c1c
Jozsef Simon <98556252+jozsefsimon22@users.noreply.github.com>
2024-06-01 12:39:15 +0200

    Update

---
+++ Resources/16.2 DOM Challenge Starting Files/index.js
@@ -0,0 +1,19 @@
+
+document.querySelector("ul").lastElementChild.innerHTML = "Jozsef Simon";
+
+document.querySelector("li a").style.color = "red";
+
+// document.querySelector("button").style.backgroundColor = "yellow";
+// document.querySelector("button").style.border = "solid white";
+// document.querySelector("button").style.height = "3rem";
+// document.querySelector("button").style.borderRadius = "5px";
+
+
+document.querySelector("button").classList.add("invisible");
+document.querySelector("button").classList.remove("invisible");
+
+
+document.querySelector("h1").innerHTML = "<er>Hi There</er>";
+document.querySelector("h1").textContent = "Good Bye";
+
+document.querySelector("a").setAttribute("href", "https://www.gamekapocs.hu")
