0d5a4606cbc8c1e85b4d828f2b35a69be8cf93b1
Jozsef Simon <98556252+jozsefsimon22@users.noreply.github.com>
2024-06-14 13:14:53 +0200

    Completed next button function

--- Projects/The Simon Game/index.js
+++ Projects/The Simon Game/index.js
@@ -1,4 +1,6 @@
 
+var currentLevel = 1;
+
 $(".box").mousedown(function () {
     $(this).addClass("pressed");
     console.log(nextButton());
@@ -9,7 +11,45 @@ $(".box").mouseup(function () {
 });
 
 
-function nextButton(){
+$(document).keydown(function (e) {
+    $("h1").text("Level " + currentLevel);
+    nextButton();
+});
+
+
+function nextButton() {
     var button = Math.floor(Math.random() * 4);
+
+
+    switch (button) {
+        case 0:
+            $(".green").addClass("pressed");
+            var greenSound = new Audio("/sounds/tom-1.mp3");
+            greenSound.play();
+            setTimeout(function () {
+                $(".green").removeClass("pressed");
+            }, 1000);
+            break;
+        case 1:
+            $(".red").addClass("pressed");
+            setTimeout(function () {
+                $(".red").removeClass("pressed");
+            }, 1000);
+            break;
+        case 2:
+            $(".yellow").addClass("pressed");
+            setTimeout(function () {
+                $(".yellow").removeClass("pressed");
+            }, 1000);
+            break;
+        case 3:
+            $(".blue").addClass("pressed");
+            setTimeout(function () {
+                $(".blue").removeClass("pressed");
+            }, 1000);
+            break;
+
+    }
+
     return button;
 }
