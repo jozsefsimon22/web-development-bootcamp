de7109b12a25ad9a39cd66e76dbd2d087ec7760d
Jozsef Simon <nex.simon@gmail.com>
2024-08-18 12:41:58 +0200

    permalist project done

--- Resources/Backend/8.6 Permalist Project/index.js
+++ Resources/Backend/8.6 Permalist Project/index.js
@@ -1,33 +1,87 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "1992",
+  port : 5432
+})
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
-];
+async function getItems(){
+  let items = []
 
-app.get("/", (req, res) => {
+  try{
+    const result = await db.query("SELECT * FROM items");
+    if(result){
+      items = result.rows
+    }
+  }
+  catch(err){
+    console.error(err)
+  }
+
+  return items;
+}
+
+app.get("/", async (req, res) => {
+  
+  let items = await getItems();
+
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
   });
 });
 
-app.post("/add", (req, res) => {
+app.post("/add", async (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
+  try{
+    await db.query("INSERT INTO items (title) VALUES ($1)",
+      [item]
+    )
+  }
+  catch (err){
+    console.log(err)
+  }
   res.redirect("/");
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", async (req, res) => {
+  const itemId = req.body.updatedItemId;
+  const itemTitle = req.body.updatedItemTitle;
 
-app.post("/delete", (req, res) => {});
+  try{
+    await db.query(`UPDATE items SET title = '${itemTitle}' WHERE id = ${itemId}`)
+  }
+  catch(err){
+    console.error(err)
+  }
+
+  res.redirect("/")
+});
+
+app.post("/delete", async (req, res) => {
+  const itemId = req.body.deleteItemId
+
+  try{
+    await db.query(`DELETE FROM items WHERE id = ${itemId}`)
+  }
+  catch(err){
+    console.error(err)
+  }
+
+  res.redirect("/")
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
